a95ada618a6c5c50a098fe966da48ca9ed93676b
Malith212 <malithnavindu130@gmail.com>
2026-03-27 09:04:58 +0530

    Strcture created

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -3,21 +3,27 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import java.time.Duration;
 
 public class GoogleTest {
 
     @Test
     public void openGoogle() {
 
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup(); // setup driver
+
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com");
-        System.out.println("Title: " + driver.getTitle());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        driver.quit();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+
+        driver.findElement(By.className("signInBtn")).click();
     }
 }
-
-//switched to the dev
