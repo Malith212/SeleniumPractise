d0a1072dace229219fd9332d6755bc695466330e
Malith212 <malithnavindu130@gmail.com>
2026-03-27 12:54:47 +0530

    section 43

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -21,11 +21,15 @@ public class GoogleTest {
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+        String password=getPassword(driver);
+
+        System.out.println(password);
+
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
 
         driver.findElement(By.id("inputUsername")).sendKeys(name);
 
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
 
         driver.findElement(By.className("signInBtn")).click();
 
@@ -40,4 +44,22 @@ public class GoogleTest {
         Thread.sleep(2000);
 
     }
+
+    public String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.xpath("//a[text()='Forgot your password?']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[text()='Reset Login']")).click();
+        Thread.sleep(2000);
+
+        String passwordText = driver.findElement(By.xpath("//p[@class='infoMsg']")).getText();
+        System.out.println(passwordText);
+
+        String password = passwordText.split("'")[1].split("'")[0];
+
+
+        System.out.println(password);
+        return password;
+    }
+
 }
